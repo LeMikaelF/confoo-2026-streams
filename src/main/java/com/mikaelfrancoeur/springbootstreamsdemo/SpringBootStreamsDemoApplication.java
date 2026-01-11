1fc635240c80d54ea02437171374655d0535789e
Mikaël Francoeur <mikael.francoeur@hotmail.com>
2026-01-11 15:33:05 -0500

    initial commit

---
+++ src/main/java/com/mikaelfrancoeur/springbootstreamsdemo/SpringBootStreamsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mikaelfrancoeur.springbootstreamsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStreamsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStreamsDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mikaelfrancoeur/springbootstreamsdemo/SpringBootStreamsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikaelfrancoeur.springbootstreamsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStreamsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
