c1f3cf8c3ffbcf923b45ecb8e7265d022f08145e
Mikaël Francoeur <mikael.francoeur@hotmail.com>
2026-01-11 22:11:28 -0500

    cleanup

--- src/main/java/com/mikaelfrancoeur/springbootstreamsdemo/config/Config.java
+++ src/main/java/com/mikaelfrancoeur/springbootstreamsdemo/config/Config.java
@@ -1,4 +1,4 @@
-package com.mikaelfrancoeur.springbootstreamsdemo;
+package com.mikaelfrancoeur.springbootstreamsdemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
